756d29c081038cc004f9c818e68964ccc4ba2b7a
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2023-02-11 19:23:14 -0300

    comparação de sting para ver se eu sou eu

---
+++ lab/rafaelNaoRafael.js
@@ -0,0 +1,11 @@
+let nameUser = `felipe`;
+
+function nameCheck(nome){
+    if(nome === `rafael`){
+        console.log(`yes, you is the ${nameUser}`);
+    } else{
+        console.log(`no you are not rafael, you are the ${nameUser}`);
+    }
+}
+
+console.log(nameCheck(nameUser));
