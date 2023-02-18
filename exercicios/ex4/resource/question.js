fa4e1f01df63695c30d646098cd9dbba9869c01f
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2023-02-18 17:14:47 -0300

    melhoramento na sintax

--- exercicios/ex4/resource/question.js
+++ exercicios/ex4/resource/question.js
@@ -1,21 +1,12 @@
-const number = Number(prompt(`Digite um numero: `));
-const numberUserIndex = document.getElementById(`numerUser`);
-const textoArea = document.getElementById(`texto`);
-const raiz = document.getElementById(`raiz`);
-const inteiro = document.getElementById(`inteiro`);
-const nan = document.getElementById(`nan`);
-const cima = document.getElementById(`cima`);
-const baixo = document.getElementById(`baixo`);
-const casa = document.getElementById(`casa`);
+const numero = Number(prompt(`Digite um numero`));
+const numeroUsuarui = document.getElementById('numerUser');
+const texto = document.getElementById('texto');
 
+numeroUsuarui.innerHTML = numero;
 
-let inteiroV = number % 1 === 0 ? `Sim` : `Não`;
-let isNan = isNaN(number) ? `Sim`:`Não`;
-
-numberUserIndex.innerHTML = number;
-raiz.innerHTML = `<p>a raz quadrada de <span>${number}</span> é <span>${Math.sqrt(number)}</span></p>`;
-inteiro.innerHTML = `<p>é um numero inteiro?<span> ${inteiroV}</span></p>`;
-nan.innerHTML = `<p> é um nan?<span> ${isNan}</span> </p>`;
-cima.innerHTML = `<p> Arredondando para cima fica: <span>${Math.ceil(number)}</span></p>`;
-baixo.innerHTML = `<p>Arredondando para baixo fica: <span>${Math.floor(number)}</span></p>`;
-casa.innerHTML = `<p>E o com duas casas depois da virgula fica:<span> ${number.toFixed(2)}</span></p>`;
+texto.innerHTML = ``;
+texto.innerHTML += `<p>A Raiz quadrada de <spna>${numero}</span> é <span>${Math.sqrt(numero)} </span></p>`
+texto.innerHTML += `<p>O numero <span>${numero}</span> é inteiro? <span> ${Number.isInteger(numero)}</span></p>`
+texto.innerHTML += `<p>Arredondando para baixo fica: <span>${Math.floor(numero)}</span></p>`
+texto.innerHTML += `<p>Arredondando para baixo fica: <span>${Math.ceil(numero)}</span></p>`
+texto.innerHTML += `<p>Com duas casas decimas fica: <span>${numero.toFixed(2)}</span></p>`
