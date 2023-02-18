988b0873be9f79bad6d30d10380f53d0da5eb823
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2023-02-18 00:00:30 -0300

    finalizando a pratica

--- exercicios/ex4/resource/question.js
+++ exercicios/ex4/resource/question.js
@@ -10,8 +10,12 @@ const casa = document.getElementById(`casa`);
 
 
 let inteiroV = number % 1 === 0 ? `Sim` : `Não`;
+let isNan = isNaN(number) ? `Sim`:`Não`;
 
 numberUserIndex.innerHTML = number;
-raiz.innerHTML = `<p>a raiz quadrada de ${number} é ${Math.sqrt(number)}</p>`
-inteiro.innerHTML = `<p>é um numero inteiro? ${inteiroV}</p>`
-
+raiz.innerHTML = `<p>a raz quadrada de <span>${number}</span> é <span>${Math.sqrt(number)}</span></p>`;
+inteiro.innerHTML = `<p>é um numero inteiro?<span> ${inteiroV}</span></p>`;
+nan.innerHTML = `<p> é um nan?<span> ${isNan}</span> </p>`;
+cima.innerHTML = `<p> Arredondando para cima fica: <span>${Math.ceil(number)}</span></p>`;
+baixo.innerHTML = `<p>Arredondando para baixo fica: <span>${Math.floor(number)}</span></p>`;
+casa.innerHTML = `<p>E o com duas casas depois da virgula fica:<span> ${number.toFixed(2)}</span></p>`;
