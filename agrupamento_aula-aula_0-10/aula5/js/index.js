1f42ee2c6e5bc9276857bf6b12ffc8ff068fd7ce
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2023-02-18 20:30:01 -0300

    oraganização2

---
+++ agrupamento_aula-aula_0-10/aula5/js/index.js
@@ -0,0 +1,4 @@
+console.log('ola,mundo');
+console.log('essa mesagem sera exibida no console do navegador');
+
+alert('ola, mundo')
---
+++ agrupamento_aula_11-20/aula12/array.js
@@ -0,0 +1,12 @@
+//            012345
+const nome = `rafael`
+//                0         1       2           3          4          5...
+let alunos = [`rafael`, `felipe`, `maria`, `fernanda`, `bruna`, `winderson`];
+
+alunos[alunos.length] = `jorjana`;
+alunos[alunos.length] = `maxiel`;
+alunos[alunos.length] = `wilian`;
+alunos[alunos.length] = `florentina`;
+
+
+console.log(alunos);
