3282e1c4d589b5060dbee735795a5f2e30a54773
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2023-02-21 08:39:35 -0300

    finalização aula12

--- agrupamento_aula_11-20/aula12/array.js
+++ agrupamento_aula_11-20/aula12/array.js
@@ -3,10 +3,24 @@ const nome = `rafael`
 //                0         1       2           3          4          5...
 let alunos = [`rafael`, `felipe`, `maria`, `fernanda`, `bruna`, `winderson`];
 
-alunos[alunos.length] = `jorjana`;
-alunos[alunos.length] = `maxiel`;
-alunos[alunos.length] = `wilian`;
-alunos[alunos.length] = `florentina`;
+// array no js é interpletado como um obejeto 
+
+// console.log(alunos.slice(0, -2)); //posso "fatiar" e escolhe um asequencia em que quero ver nesse caso vai me mostraaparti do indice 0 e nao exebir os dois ultimos indece
 
 
-console.log(alunos);
+// delete alunos[2] //delete + array[] exclui um elemento dentro do indice, porem nao exclui o indice junto
+
+
+// alunos.shift(); //assim como .pop o .shift remove, porem remove o primeiro indice
+
+// alunos.pop(); // remove o ultimo indice 
+
+
+// alunos.unshift(`matheus`) //unshift() coloca o valor no indice zero, nesse casa o rafael passa a ser indice 1 e o matheus indice 0
+
+
+// alunos.push(`agelica`) // .push() add no fim da array
+// alunos.push(`àndreia`) // .push() add no fim da array
+
+// alunos[3] = `Luizinha` // alunos[] altera o indice marcado 
+// alunos[0] = `jorge` // alunos[] altera o indice marcado nesse caso vai trocar o rafael por jorge
